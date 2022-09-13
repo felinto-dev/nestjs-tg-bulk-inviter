fb2a73ec64a6f70bbac7e8e49d3754574575be1b
Felinto <emersong20.email@gmail.com>
2022-09-13 17:43:55 +0000

    ♻️ move logic to create telegram client to accounts service

--- src/accounts/controllers/accounts.controller.ts
+++ src/accounts/controllers/accounts.controller.ts
@@ -1,7 +1,4 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { TelegramClient } from 'telegram';
-import { LogLevel } from 'telegram/extensions/Logger';
-import { StringSession } from 'telegram/sessions';
 import {
   AddAccountByBotToken,
   AddAccountByStringSession,
@@ -13,44 +10,16 @@ export class AccountsController {
   constructor(private readonly accounts: AccountsService) {}
 
   @Post('string_session')
-  async addAccountByStringSession(@Body() account: AddAccountByStringSession) {
-    const client = new TelegramClient(
-      new StringSession(account.stringSession),
-      +account.apiId,
-      account.apiHash,
-      { connectionRetries: 5 },
-    );
-    client.setLogLevel(LogLevel.NONE);
-    await client.connect();
-    const isUserAuthorized = await client.isUserAuthorized();
-
-    if (isUserAuthorized) {
-      this.accounts.addAccount({
-        client,
-        stringSession: account.stringSession,
-      });
-    }
+  async addAccountByStringSession(
+    @Body() connectionParams: AddAccountByStringSession,
+  ) {
+    const account = await this.accounts.loginByStringSession(connectionParams);
+    this.accounts.addAccount(account);
   }
 
   @Post('bot_token')
-  async addAccountByBotToken(@Body() account: AddAccountByBotToken) {
-    const client = new TelegramClient(
-      new StringSession(),
-      +account.apiId,
-      account.apiHash,
-      { connectionRetries: 5 },
-    );
-    client.setLogLevel(LogLevel.NONE);
-    await client.connect();
-    await client.start({ botAuthToken: account.botToken });
-    const isUserAuthorized = await client.isUserAuthorized();
-    const stringSession = client.session.save() as unknown as string;
-
-    if (isUserAuthorized) {
-      this.accounts.addAccount({
-        client,
-        stringSession: stringSession,
-      });
-    }
+  async addAccountByBotToken(@Body() connectionParams: AddAccountByBotToken) {
+    const account = await this.accounts.loginByBotToken(connectionParams);
+    this.accounts.addAccount(account);
   }
 }
--- src/accounts/services/accounts.service.ts
+++ src/accounts/services/accounts.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { TelegramClient } from 'telegram';
+import { LogLevel } from 'telegram/extensions/Logger';
+import { StringSession } from 'telegram/sessions';
+import {
+  AddAccountByBotToken,
+  AddAccountByStringSession,
+} from '../inputs/add-account.input';
 
 type Account = {
   client: TelegramClient;
@@ -9,6 +15,35 @@ type Account = {
 @Injectable()
 export class AccountsService {
   private readonly accounts: Account[] = [];
+
+  async loginByStringSession(account: AddAccountByStringSession) {
+    const client = new TelegramClient(
+      new StringSession(account.stringSession),
+      +account.apiId,
+      account.apiHash,
+      { connectionRetries: 5 },
+    );
+    client.setLogLevel(LogLevel.NONE);
+    await client.connect();
+    const isUserAuthorized = await client.isUserAuthorized();
+    const stringSession = client.session.save() as unknown as string;
+    return isUserAuthorized ? { client, stringSession } : null;
+  }
+
+  async loginByBotToken(account: AddAccountByBotToken) {
+    const client = new TelegramClient(
+      new StringSession(),
+      +account.apiId,
+      account.apiHash,
+      { connectionRetries: 5 },
+    );
+    client.setLogLevel(LogLevel.NONE);
+    await client.connect();
+    await client.start({ botAuthToken: account.botToken });
+    const isUserAuthorized = await client.isUserAuthorized();
+    const stringSession = client.session.save() as unknown as string;
+    return isUserAuthorized ? { client, stringSession } : null;
+  }
 
   addAccount(account: Account) {
     this.accounts.push(account);
