15c111b04c8a9646ffc30b979f0f1328755caa2a
Felinto <emersong20.email@gmail.com>
2022-09-13 15:56:27 +0000

    🚧 add support to limit the number of accounts to return

--- src/groups/controllers/groups.controller.ts
+++ src/groups/controllers/groups.controller.ts
@@ -1,4 +1,11 @@
-import { BadRequestException, Controller, Get, Param } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Param,
+  Query,
+} from '@nestjs/common';
 import { AccountsService } from '../../accounts/services/accounts.service';
 
 @Controller('groups')
@@ -6,21 +13,24 @@ export class GroupsController {
   constructor(private readonly accounts: AccountsService) {}
 
   @Get('members/:groupId')
-  async getMembers(@Param('groupId') groupUsername: number) {
+  async getMembers(
+    @Param('groupId') groupUsername: number,
+    @Query('limit', new DefaultValuePipe(10)) limit: number,
+  ) {
     const members = [];
 
     const totalNumberOfAccounts = this.accounts.countAccounts();
 
     if (totalNumberOfAccounts === 0) {
-      throw new BadRequestException('No accounts available');
+      throw new BadRequestException(
+        'No accounts found. You need to create at least one account.',
+      );
     }
 
     const accounts = this.accounts.getAccounts();
 
     for (const account of accounts) {
-      const participants = account.iterParticipants(groupUsername, {
-        limit: 10,
-      });
+      const participants = account.iterParticipants(groupUsername, { limit });
       for await (const participant of participants) {
         members.push({
           id: participant.id,
