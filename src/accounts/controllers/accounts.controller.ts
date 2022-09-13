5b32d4a7ce86aef8b258083bc5f30a16e9045184
Felinto <emersong20.email@gmail.com>
2022-09-13 17:15:09 +0000

    🏷️ update add account input

--- src/accounts/controllers/accounts.controller.ts
+++ src/accounts/controllers/accounts.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { TelegramClient } from 'telegram';
 import { LogLevel } from 'telegram/extensions/Logger';
 import { StringSession } from 'telegram/sessions';
-import { AddAccountInput } from '../inputs/add-account.input';
+import { AddAccountByStringSession } from '../inputs/add-account.input';
 import { AccountsService } from '../services/accounts.service';
 
 @Controller({ path: 'accounts' })
@@ -10,7 +10,7 @@ export class AccountsController {
   constructor(private readonly accounts: AccountsService) {}
 
   @Post('string_session')
-  async addAccountbyStringSession(@Body() account: AddAccountInput) {
+  async addAccountbyStringSession(@Body() account: AddAccountByStringSession) {
     const client = new TelegramClient(
       new StringSession(account.stringSession),
       +account.apiId,
--- src/accounts/inputs/add-account.input.ts
+++ src/accounts/inputs/add-account.input.ts
@@ -1,5 +1,12 @@
-export class AddAccountInput {
+export class App {
   apiId: number;
   apiHash: string;
+}
+
+export class AddAccountByStringSession extends App {
   stringSession: string;
+}
+
+export class AddAccountByBotToken extends App {
+  botToken: string;
 }
