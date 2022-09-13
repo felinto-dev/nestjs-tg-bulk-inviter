ccf1b4978b5feed09ff5988d7eaac3d593e42b20
Felinto <emersong20.email@gmail.com>
2022-09-13 01:20:06 +0000

    🚧 add support for commander

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { CommandFactory } from 'nest-commander';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await CommandFactory.run(AppModule);
   await app.listen(3000);
 }
 bootstrap();
