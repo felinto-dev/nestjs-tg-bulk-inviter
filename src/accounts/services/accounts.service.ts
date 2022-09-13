7b4afc5c97743145284c81f2b039ca61e65ff525
Felinto <emersong20.email@gmail.com>
2022-09-13 17:47:10 +0000

    ♻️ create service to setup telegram client

--- src/accounts/services/accounts.service.ts
+++ src/accounts/services/accounts.service.ts
@@ -5,6 +5,7 @@ import { StringSession } from 'telegram/sessions';
 import {
   AddAccountByBotToken,
   AddAccountByStringSession,
+  App,
 } from '../inputs/add-account.input';
 
 type Account = {
@@ -16,29 +17,36 @@ type Account = {
 export class AccountsService {
   private readonly accounts: Account[] = [];
 
-  async loginByStringSession(account: AddAccountByStringSession) {
+  private async setupTelegramClient(api: App, stringSession = '') {
     const client = new TelegramClient(
-      new StringSession(account.stringSession),
-      +account.apiId,
-      account.apiHash,
+      new StringSession(stringSession),
+      +api.apiId,
+      api.apiHash,
       { connectionRetries: 5 },
     );
     client.setLogLevel(LogLevel.NONE);
     await client.connect();
+    return client;
+  }
+
+  async loginByStringSession(account: AddAccountByStringSession) {
+    const client = await this.setupTelegramClient(
+      {
+        apiId: account.apiId,
+        apiHash: account.apiHash,
+      },
+      account.stringSession,
+    );
     const isUserAuthorized = await client.isUserAuthorized();
     const stringSession = client.session.save() as unknown as string;
     return isUserAuthorized ? { client, stringSession } : null;
   }
 
   async loginByBotToken(account: AddAccountByBotToken) {
-    const client = new TelegramClient(
-      new StringSession(),
-      +account.apiId,
-      account.apiHash,
-      { connectionRetries: 5 },
-    );
-    client.setLogLevel(LogLevel.NONE);
-    await client.connect();
+    const client = await this.setupTelegramClient({
+      apiId: account.apiId,
+      apiHash: account.apiHash,
+    });
     await client.start({ botAuthToken: account.botToken });
     const isUserAuthorized = await client.isUserAuthorized();
     const stringSession = client.session.save() as unknown as string;
