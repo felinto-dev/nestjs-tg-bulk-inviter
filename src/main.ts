a5aa265a28508eef24b6a1f161b3d3104768c14f
Felinto <emersong20.email@gmail.com>
2022-09-13 01:40:08 +0000

    🚧 remove nestjs-commander

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { CommandFactory } from 'nest-commander';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await CommandFactory.run(AppModule);
   await app.listen(3000);
 }
 bootstrap();
